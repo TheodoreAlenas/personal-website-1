7d7cffaf1ecb0f1edc859b6e46cb3764cd46fda2
Theodore Alenas <dimakopt732@gmail.com>
2023-07-08 23:43:59 +0300

    refactor

--- common/menu-bar/mod.php
+++ common/menu-bar/mod.php
@@ -11,32 +11,40 @@ function get_menu_bar_css() {
 }
 
 function get_menu_bar(string $title, string $language) {
-  $hamid = random_int(1, 1000);
+  $li = wrap_in_li([
+      get_a_tag("portfolio", $language),
+      get_a_tag("biography", $language),
+      get_a_tag("contact", $language)
+    ]);
   return
     "<header class='menu-bar'><nav>" .
     get_a_tag("home", $language) .
     "<span>$title</span>" .
-    wrap_in_list("<ul class='desktop-nav-list'>", "</ul>", [
-      get_a_tag("portfolio", $language),
-      get_a_tag("biography", $language),
-      get_a_tag("contact", $language)
-    ]) .
-    "<input id='hamburger-checkbox-$hamid' type='checkbox' class='hamburger-checkbox' />" .
-    "<label for='hamburger-checkbox-$hamid' class='hamburger-label'></label>" .
-    wrap_in_list("<ul class='hamburger-nav-list'>", "</ul>", [
-      get_a_tag("portfolio", $language),
-      get_a_tag("biography", $language),
-      get_a_tag("contact", $language)
-    ]) .
+    "<ul class='desktop-nav-list'>$li</ul>" .
+    "<ul class='hamburger-nav-list'>$li</ul>" .
+    get_hamburger_checkbox_pair() .
     "</nav></header>";
 }
 
-function wrap_in_list(string $opening_tag, string $closing_tag, array $components) {
-  $e = $opening_tag;
+function get_hamburger_checkbox_pair() {
+  $hamburger_hash = random_int(1, 1000);
+  return
+    "<input" .
+    " id='hamburger-checkbox-$hamburger_hash'" .
+    " type='checkbox'" .
+    " class='hamburger-checkbox'" .
+    "/>" .
+    "<label" .
+    " for='hamburger-checkbox-$hamburger_hash'" .
+    " class='hamburger-label'" .
+    "></label>";
+}
+
+function wrap_in_li(array $components) {
+  $e = '';
   foreach ($components as $component) {
     $e .= "<li>$component</li>";
   }
-  $e .= $closing_tag;
   return $e;
 }
 
