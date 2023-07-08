14132ad4261ba8250d377cf075dca4cd634a61f8
Theodore Alenas <dimakopt732@gmail.com>
2023-07-08 15:16:30 +0300

    couldn't resist to do a bit more

--- common/test.php
+++ common/test.php
@@ -3,19 +3,19 @@
 function get_wrapped_in_asymmetric_grid(String $component) {
 
   return '<div class="test-wrapper">' .
-    '<div class="main-container test-layout-tall phone-screen outline">' .
+    '<div class="main-container test-layout-tall phone-screen">' .
   $component .
   '</div>' .
 
-  '<div class="main-container test-layout-almost outline">' .
+  '<div class="main-container test-layout-almost">' .
   $component .
   '</div>' .
 
-  '<div class="main-container test-layout-below-half outline">' .
+  '<div class="main-container test-layout-below-half phone-screen">' .
   $component .
   '</div>' .
 
-  '<div class="main-container test-layout-above-half phone-screen outline">' .
+  '<div class="main-container test-layout-above-half">' .
   $component .
   '</div>' .
 
