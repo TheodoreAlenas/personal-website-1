965b0dbdb095bea3f4aeb8357be49bf401006362
Theodore Alenas <dimakopt732@gmail.com>
2023-07-10 00:31:41 +0300

    refactored the story opening

--- home/story/mod.php
+++ home/story/mod.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once(__DIR__ . "/opening.php");
+
 function get_story_and_css(string $language) {
   return [
     get_story($language),
@@ -15,48 +17,23 @@ function get_story(string $language) {
 }
 
 function get_story_en() {
-  return get_story_opening_en();
+  return
+    get_story_opening_en().
+    get_story_opening_en().
+    get_story_opening_en().
+    get_story_opening_en().
+    get_story_opening_en().
+    get_story_opening_en();
 }
 
 function get_story_gr() {
-  return get_story_opening_gr();
-}
-
-function get_story_opening_en() {
-  return <<<EOHTML
-<p>
-<i>
-&quot;Everyone knows the definition of a programmer.
-A guy in a room, you throw meat
-and programs come out.&quot;
-</i>
-</p>
-<p>
-That's from a
-<a href="#">talk</a> by
-<a href="#">Robert C. Martin</a>.
-EOHTML;
-}
-
-function get_story_opening_gr() {
-  return <<<EOHTML
-<p>
-Μια από τις φράσεις του κ.
-<a href="#">Robert C. Martin</a>
-που με συγκίνησαν περισσότερο είναι
-</p>
-<p>
-<i>
-&quot;Και ποιος είναι ο ορισμός του προγραμματιστή;
-Όλοι γνωρίζουμε τον ορισμό του προγραμματιστή.
-Ένας τύπος, κλεισμένος, πετάς κρέας
-και βγαίνουν προγράμματα.&quot;
-</i>
-</p>
-<p>
-που άκουσα σε μια
-<a href="#">ομιλία</a> του.
-EOHTML;
+  return
+    get_story_opening_gr().
+    get_story_opening_gr().
+    get_story_opening_gr().
+    get_story_opening_gr().
+    get_story_opening_gr().
+    get_story_opening_gr();
 }
 
 ?>
