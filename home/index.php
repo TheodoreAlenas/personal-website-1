dda52819d72ed03f77aca30961339d50aeecd7a8
Theodore Alenas <dimakopt732@gmail.com>
2023-07-09 19:08:45 +0300

    added some title to index.html (not sure about it)

--- home/index.php
+++ home/index.php
@@ -6,7 +6,7 @@ include_once("common/menu-bar/mod.php");
 
 $basic_css = ["common/html-body.css", "common/centering.css"];
 
-[$menu_bar, $menu_bar_css] = get_menu_bar_and_css("", "gr");
+[$menu_bar, $menu_bar_css] = get_menu_bar_and_css("Theodore", "gr");
 [$banner, $banner_css] = get_banner_and_css("gr");
 
 $css_to_include = array_merge($basic_css, $menu_bar_css, $banner_css);
