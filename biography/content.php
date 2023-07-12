138c7188b8260c14470dcfae620b7f7bf5704c4a
Theodore Alenas <dimakopt732@gmail.com>
2023-07-12 21:54:42 +0300

    started the bio

--- biography/content.php
+++ biography/content.php
@@ -1,7 +1,40 @@
 <?php
 
 function get_content_and_css(string $language) {
-  return ["biography content, langended in $language language.", []];
+  if ($language == "gr") {
+    return [get_bio_school_gr(), []];
+  }
+  return [get_bio_school_en(), []];
+}
+
+
+function get_bio_school_en() {
+  return <<<EOHTML
+
+<p>
+Studied <b>sciences</b> in secondary school in Greece
+(math, physics, chemistry,
+reading and writing formal essays)
+and passed with 17325/20000 in 2019.
+2019 is a year known for the extra difficulty
+in the final exams.
+</p>
+
+EOHTML;
+}
+
+function get_bio_school_gr() {
+  return <<<EOHTML
+
+<p>
+Ολοκλήρωσα σε Γενικό Λύκειο
+σε κατεύθυνση <b>θετικών επιστημών</b>
+με βαθμολογία 17325 μόρια στις Πανελλαδικές εξετάσεις,
+το 2019.
+Το 2019 ήταν έτος με αρκετά δύσκολες εξετάσεις.
+</p>
+
+EOHTML;
 }
 
 ?>
--- biography/mod.php
+++ biography/mod.php
@@ -24,10 +24,12 @@ function get_biography_html(string $language) {
 
   return
     get_top_of_file("Theodore - Home", $css_to_include) .
-    "<header class='free-media-query sticky'>" .
+    "<header class='free-media-query'>" .
     $menu_bar .
     "</header>" .
+    "<main class='free-media-query newspaper lrline p2'>" .
     $content .
+    "</main>" .
     get_bottom_of_file();
 }
 
