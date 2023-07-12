b035904a867e9260269d0dbc131c325d4b561f90
Theodore Alenas <dimakopt732@gmail.com>
2023-07-12 14:35:34 +0300

    don't need the interactivity.css

--- common/css/mod.php
+++ common/css/mod.php
@@ -14,7 +14,6 @@ function get_css_snippet_files() {
     "common/css/margin-padding.css",
     "common/css/width-border.css",
     "common/css/display-position.css",
-    "common/css/interactivity.css",
   ];
 }
 
