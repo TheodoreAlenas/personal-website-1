1da17f8809b49f5db1b7c2f1a79cdba7bd2b2af0
Theodore Alenas <dimakopt732@gmail.com>
2023-07-13 23:46:32 +0300

    used an awkwardly minimal style for contact

--- contact/mod.php
+++ contact/mod.php
@@ -3,7 +3,18 @@
 include_once("common/wraps/typical-layouts.php");
 
 function get_contact_html(string $language) {
-  return get_typical_layout($language, "contact", [], "", "hello");
+  return get_typical_layout(
+    $language, "contact", [], "",
+    <<<EOHTML
+
+<h2>E-mail</h2>
+<pre style='text-align: center;'>dimakopt732@gmail.com</pre>
+
+<h2>Phone</h2>
+<pre style='text-align: center;'>+30 693 975 1642</pre>
+
+EOHTML
+  );
 }
 
 ?>
