7b46838d1b523d3c17932c137a46e834845017c0
Theodore Alenas <dimakopt732@gmail.com>
2023-10-14 10:34:32 +0300

    CSS file origins appear in the dev tools

--- common/wraps/ends-of-main-html.php
+++ common/wraps/ends-of-main-html.php
@@ -16,12 +16,14 @@ function get_top_of_file(String $title, array $css_files) {
 
     <style>
 
-EOHTML . get_all_file_contents($css_files) . <<<EOHTML
+EOHTML . get_all_file_contents($css_files, "/* from ", " */\n") .
+         <<<EOHTML
 
     </style>
     <script>
 
-EOHTML . get_all_file_contents(["common/light-dark-theme/functions.js"]) . <<<EOHTML
+EOHTML . get_all_file_contents(["common/light-dark-theme/functions.js"],
+                          "/* from ", " */\n") . <<<EOHTML
 
     </script>
 
@@ -48,7 +50,27 @@ function get_bottom_of_file() {
 EOHTML;
 }
 
-function get_all_file_contents(array $file_names) {
+$PART_OF_LICENSE = <<<EOPART_OF_LICENSE
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+EOPART_OF_LICENSE;
+
+function get_all_file_contents(array $file_names,
+                               string $comment_start,
+                               string $comment_end) {
   $to_return = "";
 
   foreach (array_unique($file_names) as $file_name) {
@@ -56,8 +78,28 @@ function get_all_file_contents(array $file_names) {
     if ($file == false) {
       continue;
     }
-    $to_return .= fread($file, 1048576);
+    $read = fread($file, 1048576);
     fclose($file);
+    $to_return .= $comment_start . $file_name . $comment_end .
+               str_replace(<<<EOPART_OF_LICENSE
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+EOPART_OF_LICENSE,
+                           "[$file_name goes on]",
+                           $read);
   }
 
   return $to_return;
